38cdc5546a0efe646ca7b3a7ef9dcbb8e9ce77e8
Jay Hsia <jayhsia1997@gmail.com>
2026-06-02 16:32:14 -0400

    feat: Implement account management and transaction functionality
    - Add API endpoints for creating and retrieving accounts
    - Introduce transaction handling for money transfers between accounts
    - Update database store to support transaction execution
    - Enhance main application to connect to PostgreSQL and start the server
    - Include necessary dependencies in go.mod and go.sum for new features

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -2,12 +2,15 @@ package db
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+// Store defines all functions to execute db queries and transactions
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
 
 // SQLStore provides all functions to execute SQL queries and transactions
 type SQLStore struct {
@@ -16,79 +19,9 @@ type SQLStore struct {
 }
 
 // NewStore creates a new store
-func NewStore(connPool *pgxpool.Pool) *SQLStore {
+func NewStore(connPool *pgxpool.Pool) Store {
 	return &SQLStore{
 		connPool: connPool,
 		Queries:  New(connPool),
 	}
-}
-
-func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
-	tx, err := store.connPool.BeginTx(ctx, pgx.TxOptions{})
-	if err != nil {
-		return err
-	}
-	q := New(tx)
-	err = fn(q)
-	if err != nil {
-		if rbErr := tx.Rollback(ctx); rbErr != nil {
-			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
-		}
-		return err
-	}
-
-	return tx.Commit(ctx)
-}
-
-type TransferTxParams struct {
-	FromAccountID int64 `json:"from_account_id"`
-	ToAccountID   int64 `json:"to_account_id"`
-	Amount        int64 `json:"amount"`
-}
-
-type TransferTxResult struct {
-	Transfer    Transfer `json:"transfer"`
-	FromAccount Account  `json:"from_account"`
-	ToAccount   Account  `json:"to_account"`
-	FromEntry   Entry    `json:"from_entry"`
-	ToEntry     Entry    `json:"to_entry"`
-}
-
-// TransferTx performs a money transfer from one account to the other.
-// It creates a transfer record, add account entries, and update accounts' balance within a single database transaction.
-func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
-	var result TransferTxResult
-
-	err := store.execTx(ctx, func(q *Queries) error {
-		var err error
-		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
-			FromAccountID: pgtype.Int8{Int64: arg.FromAccountID, Valid: true},
-			ToAccountID:   pgtype.Int8{Int64: arg.ToAccountID, Valid: true},
-			Amount:        arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
-			AccountID: pgtype.Int8{Int64: arg.FromAccountID, Valid: true},
-			Amount:    -arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
-			AccountID: pgtype.Int8{Int64: arg.ToAccountID, Valid: true},
-			Amount:    arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		// TODO: update accounts' balance
-
-		return nil
-	})
-	return result, err
 }
--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jayhsia1997/simplebank/api"
+	db "github.com/jayhsia1997/simplebank/db/sqlc"
 )
 
-// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+const (
+	dbSource      = "postgresql://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+func main() {
+	connPool, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(connPool)
+	server := api.NewServer(store)
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
 	}
 }
